151cb13893fa0abb0412c4d8e37051a4eae55a80
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-11-29 21:28:51 +0530

    day 15 final commit

---
+++ day15/Datatypes.js
@@ -0,0 +1,19 @@
+//Datatypes and variabes
+let str = "Hello, World!";
+let num = 42;
+let bool = true;
+let nul = null;
+let undef;
+let obj = { name: "Zaheer", age: 20 };
+let arr = [1, 2, 3, 4, 5];
+let sym = Symbol("id");
+let bigInt = BigInt(12345678901234567890);
+console.log("String: ", "Hello, World!");
+console.log("Number: ", 42);
+console.log("Boolean: ", true);
+console.log("Null: ", null);
+console.log("Undefined: ", undefined);
+console.log("Object: ", { name: "Zaheer", age: 20 });
+console.log("Array: ", [1, 2, 3, 4, 5]);
+console.log("Symbol: ", Symbol("id"));
+console.log("BigInt: ", BigInt(12345678901234567890));
