2702c092d5380cd7ac2a42eabf926e201037b31d
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-11-29 20:47:34 +0530

    day16 Operators commit

--- day16/operators.js
+++ day16/operators.js
@@ -1,1 +1,36 @@
-alert("Hello World")
+/*
+1. Arthematic Opeartors
+2. Assignment Operators
+3. Comparision Operators
+4. Logical Operators
+5. Bitwise
+6. String
+7. Miscellanous
+*/
+
+//1. Arthematic Opeartors
+//+,-,*,/,%,++,--,**
+
+var num1=5
+
+console.log("Additon:",num1+3)
+console.log("Subtraction:",num1-3)
+console.log("Multiplication:",num1*3)
+console.log("Division:",num1/3)
+console.log("Modular:",num1%3)
+console.log("Increment:",num1++) //5, bcz the value is chnaged to 6 in variable after printing the value 5.
+console.log("Pre Increment:",++num1) //7, here the value is incremented first and then displayed the value 7.
+console.log("Pre increment:",--num1)
+console.log("Post increment:",num1--)
+console.log("Exponentation:",num1**3)
+
+//2. Assignment Operators
+//=, +=, -+, *=, /=, %=, **=
+
+console.log("equal Assignment:",num1)
+console.log("Addition Assignment :",num1+=3)
+console.log("Subtraction Assignment :",num1-=3)
+console.log("Multiplication Assignment :",num1*=3)
+console.log("Division Assignment :",num1/=3)
+console.log("Modular Assignment :",num1%=3)
+console.log("Exponential Assignment :",num1**=3)
