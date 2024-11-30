e65490295e9fc7a80510e760ba58f29c4beaa2bf
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-11-30 20:33:06 +0530

    day17 third commit

--- day17/controlStatements.js
+++ day17/controlStatements.js
@@ -11,3 +11,26 @@ if(_marks>24){
 else{
     console.log("Fail")
 }
+//netsed if else blocks
+
+var _myInternalMarks=16;
+var _myExternalMarks=24;
+
+//pass = Internal Marks + External Marks >= 40
+// Internal marks >= 16
+// External marks >= 24
+
+if(_myInternalMarks+_myExternalMarks>=40){
+    if(_myInternalMarks>=16){
+        if(_myExternalMarks>=24){
+            console.log("Congrats you passed in exam!!")
+        }else{
+            console.log("Student is failed")
+        }
+    }else{
+        console.log("Student is failed")
+    }
+}
+else{
+    console.log("Student is failed")
+}
