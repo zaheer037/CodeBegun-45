99f7bb14cbaf4250bc87edfbe6041252584f6389
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-12-02 21:08:40 +0530

    Initial commit

--- day18/function.js
+++ day18/function.js
@@ -1,11 +1,7 @@
-function addTwoNumbers(){
+function calculateNumbers(){
     var _firstValue=document.getElementById('firstvalue').value;
-    var _secondvalue=document.getElementById('secondvalue').value;
+    var _secondValue=document.getElementById('secondvalue').value;
 
-    var _result=parseInt(_firstValue)+parseInt(_secondvalue); //browser considers intially all values as strings so, we parseInt()
+    
 
-    console.log(_result)
-
-    document.getElementById('result').innerHTML=_results;
 }
-
