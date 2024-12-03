caf94c1f1f290289aed45be991cb3367fdbcf456
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-12-03 20:43:31 +0530

    day19 Initial commit

--- day19/hoisting.js
+++ day19/hoisting.js
@@ -1,3 +1,47 @@
-//
-console.log(a);
-var a=10;
+//variable hoisting
+// console.log(a);
+// var a=10;
+
+
+//var a;
+// console.log(a)
+//a=10;
+
+// var a=10;
+
+// function callMe(){
+//     var a=5
+//     console.log("Inside function:"+a);
+// }
+// console.log("Outside Function:"+a);
+// callMe();
+
+
+// var a=1;
+// function ex(){
+//     var a=2;
+//     if(true){
+//         a=3; //overrides the a value as 3 bcz, var is local scope, not block level scope
+//         console.log("console 1 is:"+a);
+//     }
+//     console.log("console 2 is :"+a);
+// }
+// console.log("console 3 is :"+a);
+// ex();
+
+//var keyword is local scope , it is not a block level scope
+
+// var and let
+
+// var is function scoped, // let is block(if,while,for) scoped
+
+// var allows redeclaration //let does not allow redeclaration
+
+// var allows hoisting  // let does not allow hoisting
+
+
+// console.log(a);
+// var a;
+
+// console.log(b);
+// let b;
