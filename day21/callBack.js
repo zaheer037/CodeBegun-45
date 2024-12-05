6473016beafa4b07b4021e45cac88438decae311
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-12-05 21:11:11 +0530

    Day21 final commit

--- day21/callBack.js
+++ day21/callBack.js
@@ -1,15 +1,15 @@
-function addTwoNumbers(){
-    setTimeout(function(){
-        console.log("Adding two numbers");
-        callme()
-    },2000)
-}
+// function addTwoNumbers(){
+//     setTimeout(function(){
+//         console.log("Adding two numbers");
+//         callme()
+//     },2000)
+// }
 
-function callme(){
-    console.log("Called")
-}
+// function callme(){
+//     console.log("Called")
+// }
 
-addTwoNumbers();
+// addTwoNumbers();
 
 
 // callback hell - imp for interview
@@ -21,3 +21,22 @@ addTwoNumbers();
 //async and await
 
 //js will executes code in a synchronous way, so inorder to implement asynchronous model we moved to promises(async,await)
+
+
+function step1() {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            var x = true;
+            if (x) {
+                resolve("step 1 is completed");
+            } else {
+                reject("error");
+            }
+        }, 2000)
+    })
+}
+step1().then(value => {
+    console.log(value);
+}).catch(error => {
+    console.log(error);
+});
