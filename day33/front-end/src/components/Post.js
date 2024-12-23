432a9aff17ddf4a8c7bd4028a3983bad4caca9fc
MASEED ZAHEER <jaheermaseed@gmail.com>
2024-12-23 20:54:47 +0530

    day34 final commit

--- day33/front-end/src/components/Post.js
+++ day33/front-end/src/components/Post.js
@@ -3,11 +3,14 @@ import { useEffect,useState } from 'react'
 import axios from "axios"
 const Post = () => {
     const [data,setData] = useState([]);
+    const [load,setLoad] = useState(true);
     useEffect(()=>{
+        if (load===true){<h2>Loading...</h2>}
         const fetchData=async()=>{
             try {
                 const response=await axios.get("https://jsonplaceholder.typicode.com/posts");
                 console.log(response);
+                setLoad(false);
                 setData(response.data);
             } catch (error) {
                 
@@ -21,7 +24,7 @@ const Post = () => {
         <h1>fetch Data from API and show the data</h1>
         <ul>
             {data.slice(0,10).map((item)=>(
-                <li key={item.id}>{item.titile}</li>
+                <li key={item.id}>{item.title}</li>
             ))}
         </ul>
     </div>
